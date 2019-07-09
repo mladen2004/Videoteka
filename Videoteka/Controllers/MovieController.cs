02d5a33541e5c49c718d03f377a276fe9bbc8277
mladen2004 <48154793+mladen2004@users.noreply.github.com>
2019-07-09 23:15:29 +0200

    Add project files.

---
+++ Videoteka/Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Videoteka.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult gh()
+        {
+            return View();
+        }
+    }
+}
